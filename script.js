0a1cb2770be9da084509b8b94b58f62b5d5ce6c9
Pedro <phmc@ic.ufal.br>
2021-11-24 09:41:54 -0300

    atualizando os arquivos

--- script.js
+++ script.js
@@ -1,42 +1,41 @@
 const buttonCriar = document.querySelector('#criar-tarefa');
 const input = document.querySelector('#texto-tarefa');
 const ol = document.querySelector('#lista-tarefas');
+let li = document.querySelectorAll('.listUnselected');
 
 buttonCriar.addEventListener('click', addTask);
-input.addEventListener('keyup', addTaskEnter);
 
 function addTask() {
   if (input.value.length > 0) {
     const li = document.createElement('li');
     li.innerText = input.value;
-    ol.appendChild(li);
+    li.className = 'listUnselected';
     input.value = '';
-  }
-}
-
-function addTaskEnter(event) {
-  if (event.key === 'Enter' && input.value.length > 0) {
-    const li = document.createElement('li');
-    li.innerText = input.value;
     ol.appendChild(li);
-    input.value = '';
+  }
+
+ol.addEventListener('click', selected);
+
+function selected(event){
+  if(event.target.className.indexOf('listUnselected') !== -1) {
+    const selected = document.querySelectorAll('.listUnselected');
+    if (selected.length > 0) {
+      selected[0].classList.remove('listUnselected');
+      event.target.classList.add('listSelected');
+    }
   }
 }
 
-
-const li = document.querySelectorAll('li');
-console.log(li)
-for (let key of li) {
-  key.addEventListener('click', colorModific);
-  function colorModific(event) {
-    event.target.style.backgroundColor = rgb(128, 128, 128)
-  }
 }
+// for (let key of li) {
+//   key.addEventListener('click', selected);
+//   function selected(event) {
+//     key.classList.add('listSelected');
+//   }
+// }
+// const TaskList = document.querySelectorAll('li');
 
-
-const TaskList = document.querySelectorAll('li');
-
-for (const key of TaskList) {
-    const element = key;
-    element.remove();
-}
+// for (const key of TaskList) {
+//     const element = key;
+//     element.remove();
+// }
