c4f3a0f041921cba3b2ad548ef58ae0d721780fb
Pedro <phmc@ic.ufal.br>
2021-11-24 22:57:36 -0300

    Requisito 11 com problema e 14 passando

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
+// Variáveis
 const buttonCriar = document.querySelector('#criar-tarefa');
 const input = document.querySelector('#texto-tarefa');
 const ol = document.querySelector('#lista-tarefas');
 const buttonApagar = document.querySelector('#apaga-tudo');
+const buttonDeleteFinal = document.getElementById('remover-finalizados');
+const buttonSaveTasks = document.getElementById('salvar-tarefas');
+const buttonRemoveSelect = document.getElementById('remover-selecionado');
 
+// Criação de tarefa
 buttonCriar.addEventListener('click', addTask);
-
 function addTask() {
   if (input.value.length > 0) {
     const li = document.createElement('li');
@@ -13,6 +17,8 @@ function addTask() {
     ol.appendChild(li);
   }
 }
+
+// Atividades selecionadas
 ol.addEventListener('click', colorTask);
 function colorTask(event) {
   const selected = document.querySelector('.listSelected');
@@ -24,6 +30,7 @@ function colorTask(event) {
   }
 }
 
+// Atividades finalizadas
 let verificador = false;
 ol.addEventListener('dblclick', colorList);
 function colorList(event) {
@@ -36,6 +43,7 @@ function colorList(event) {
   }
 }
 
+// Botão apagar tudo
 let list = document.getElementsByTagName('li');
 buttonApagar.addEventListener('click', deleteList);
 function deleteList() {
@@ -44,3 +52,33 @@ function deleteList() {
     ol.removeChild(element);
   }
 }
+
+// Botão apagar selecionado
+let finalizados = document.getElementsByClassName('completed');
+buttonDeleteFinal.addEventListener('click', deleteFinal);
+function deleteFinal() {
+  for(let index = finalizados.length-1; index >= 0; index -= 1) {
+    let elementFinal = finalizados[index];
+    ol.removeChild(elementFinal);
+  }
+}
+
+// Botão apagar finalizados
+let selecionados = document.getElementsByClassName('listSelected');
+buttonRemoveSelect.addEventListener('click', deleteFinal);
+function deleteFinal() {
+  for(let index = selecionados.length-1; index >= 0; index -= 1) {
+    let elementSelected = selecionados[index];
+    ol.removeChild(elementSelected);
+  }
+}
+
+/* // Botão salvar lista
+buttonSaveTasks.addEventListener('click', saveTasks);
+function saveTasks() {
+  localStorage.setItem('tasks', JSON.stringify(list));
+}
+//localStorage.clear();
+// Chamada de função;
+let ListSave = localStorage.getItem('tasks', JSON.parse(list));
+    ol.parentNode(ListSave); */
